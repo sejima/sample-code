379cbe6181d2e7548feaeb847745417f996057e8
SEJIMA Takanori <takanori.sejima@gmail.com>
2015-09-23 12:15:56 +0900

    xxx

---
+++ 201509/test2.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int
+main(int argc, char **argv)
+{
+  enum { NUM = 256*1024*1024, };
+
+  int i, j;
+  unsigned long long int a;
+  char *p = (char *)malloc(NUM);
+
+  memset(p, 0x0a, NUM);
+
+  a = 0;
+  for (i = 0; i < 256; i++)
+    for (j = 0; j < NUM/4096; j++)
+      if (memcmp(p, p+j*4096, 4096) == 0)
+	++a;
+
+  printf("%llu\n", a);
+
+  free(p);
+
+  return 0;
+}
